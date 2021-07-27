fd07b17fc4f6b8d64309645515a1a477fc8955b6
Felipe Gabriel <liperesende1@hotmail.com>
2021-07-27 14:40:18 -0300

    Implementação das paginas blog, home e contato

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
+const { urlencoded } = require('express')
 const express = require('express')
 const server = express()
 
-server.get('/', (req, res) => {
-    res.send('Olá devFe')
-});
+server.use(express.static('src/public'))
+server.use(express.json())
+server.use(express.urlencoded({extend: false}))
 
 server.get('/manutencao', (req, res) => {
     res.sendFile(__dirname + "/src/views/manutencao.html");
 });
 
+server.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/views/home.html');
+})
 
+server.get('/home', (req, res) => {
+    res.sendFile(__dirname + "/src/views/home.html")
+})
+
+server.get('/blog', (req, res) => {
+    res.sendFile(__dirname + "/src/views/blog.html")
+})
+
+server.get('/contato', (req, res) => {
+    res.sendFile(__dirname + "/src/views/contato.html")
+})
+
+server.post('/receber-contato', (req, res) => {
+    console.log(req.body)
+    res.send("Contato recebido")
+})
 server.listen(5000, () => console.log('Ouvindo...'))
